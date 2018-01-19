bf5faaa903bcabf1e6cb7ac770bbb49def044c3d
Ishak Mahamoud <ishakmahamoud@gmail.com>
2018-01-18 16:11:23 -0800

    Update aliases file (url-encode, reload, etc.)

--- aliases.zsh
+++ aliases.zsh
@@ -20,4 +20,10 @@ alias path='echo -e ${PATH//:/\\n}'
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
 # keep homebrew clean and updated
-alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
+alias brewup='sudo softwareupdate -i -a; brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload="exec ${SHELL} -l"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
